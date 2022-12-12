94d77ce6600af7c29b1ad0c67f1b96a1c50c084c
bobbycrooz <idrisloove@gmail.com>
2022-12-12 03:47:42 +0100

    hello world

--- pages/attendance/index.jsx
+++ pages/attendance/index.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 import moment from 'moment';
+import Pattern from './pattern';
+import Countdown from 'react-countdown';
 
 const Home = () => {
 	const headerRow = ['S.No.', 'name', 'matric', 'time', 'file'];
 
 	const [newData, seetNewData] = useState({});
 	const [activeForm, setActveForm] = useState(false);
+	const [countdown, showCountDown] = useState(false);
 	const [deadlineHour, setHour] = useState();
 	const [deadlineMinues, setMinues] = useState();
+	const [formatedDateString, setString] = useState('');
 	const [studentData, setStudentData] = useState([
 		// {
 		// 	name: 'bobby',
@@ -25,6 +29,15 @@ const Home = () => {
 		} else {
 			setActveForm(false);
 		}
+
+		typeof window !== undefined &&
+			setString(
+				`${cYear}-${cMonth}-${cDay}T${deadlineHour.length == 1 ? `0${deadlineHour}` : deadlineHour}:${
+					deadlineMinues.length == 1 ? `0${deadlineMinues}` : deadlineMinues
+				}:00`
+			);
+
+		showCountDown(true);
 	}
 
 	function cancleSession() {
@@ -67,6 +80,8 @@ const Home = () => {
 		const dl = localStorage.getItem('deadlineTime');
 		if (dl) {
 			setActveForm(true);
+		} else {
+			setActveForm(false);
 		}
 	}, [deadlineHour, setDeadlineTime]);
 
@@ -93,11 +108,27 @@ const Home = () => {
 
 			setStudentData([...studentData, newStudenInfo]);
 		} else {
-			localStorage.removeItem('deadlineTime');
+			cancleSession();
+			// localStorage.removeItem('deadlineTime');
 			window.alert('not accepting submission');
-			setActveForm(false);
+
+			// setActveForm(false);
 		}
 	}
+
+	const renderer = ({ hours, minutes, seconds, completed }) => {
+		if (completed) {
+			// Render a completed state
+			return <h1>form closed</h1>;
+		} else {
+			// Render a countdown
+			return (
+				<h1 className="italic text-xl font-bold">
+					{hours}:{minutes}:{seconds}
+				</h1>
+			);
+		}
+	};
 
 	return (
 		<>
@@ -108,11 +139,12 @@ const Home = () => {
 							<h1 className="logo-text text-xl"> REPORT SUBMISSON PORTAL</h1>
 						</div>
 						<div className="hidden md:flex md:middle time  space-x-2">
-							<h1 className="deadline capitalize text-sm">accepting report till :</h1>
-
-							<h1 className="italic text-xl font-bold">
-								{deadlineHour} : {deadlineMinues}
-							</h1>
+							{countdown && (
+								<Countdown
+									date={new Date(String(formatedDateString))}
+									renderer={renderer}
+								/>
+							)}
 						</div>
 					</nav>
 
@@ -141,12 +173,21 @@ const Home = () => {
 							</h1>
 						</div>
 
-						<button
-							onClick={cancleSession}
-							className="submit_btn shadow-md hover:shadow-none capitalize p-2 px-4 bg-blue-700 rounded-lg text-white"
-						>
-							cancle deadline
-						</button>
+						<div className="middle space-x-3">
+							<button
+								onClick={cancleSession}
+								className="submit_btn shadow-md hover:shadow-none capitalize p-2 px-4 bg-blue-700 rounded-lg text-white"
+							>
+								cancle deadline
+							</button>
+
+							{countdown && (
+								<Countdown
+									date={new Date(String(formatedDateString))}
+									renderer={renderer}
+								/>
+							)}
+						</div>
 
 						<main className="student_list border shadow-sm overflow-x-scroll md:overflow-x-hidden overflow-y-scroll h-[250px]">
 							<table className=" w-[800px] md:w-full relative">
@@ -174,20 +215,23 @@ const Home = () => {
 										))}
 									</tbody>
 								) : (
-									<></>
+									<div className="absolute md:empty-list  text-yellow-600 p-6 rounded-lg top-[200%] border border-yellow-500 shadow-sm ">
+										<h1>No submission has been made</h1>
+									</div>
 								)}
 							</table>
 						</main>
 
 						<div className="action_center border bg-blue-50 rounded-md shadow   w-full p-8">
 							<form onSubmit={(e) => submitHandler(e)} className=" w-full stack">
-								<div className="md:hidden middle time  space-x-2">
-									<h1 className="deadline capitalize ">accepting report till :</h1>
-
-									<h1 className="italic text-xl font-bold">
-										{deadlineHour} : {deadlineMinues}
-									</h1>
-								</div>
+								{/* <div className="md:hidden middle time  space-x-2">
+									{countdown && (
+										<Countdown
+											date={new Date(String(formatedDateString))}
+											renderer={renderer}
+										/>
+									)}
+								</div> */}
 
 								<h1 className="time capitalize">
 									current time: {cHour}:{cMinutes}
@@ -241,15 +285,15 @@ const Home = () => {
 					</section>
 				</div>
 			) : (
-				<div className="group h-screen w-screen centered p-4">
-					<div className="card bg-blue-50 shadow-xl w-auto border p-4 h-auto rounded-lg stack">
+				<div className="group h-screen bg-blue-200 w-screen centered p-4 setDeadLine relative">
+					<div className="card z-20 bg-white shadow-xl w-auto border p-4 h-auto rounded-lg stack">
 						<h1 className="title text-lg md:text-2xl font-semibold m-2 text-gray-600 capitalize md:middle stack">
 							set deadline for report submisson
 						</h1>
 						<hr />
 
 						<div className="w-full stack p-1 space-y-2">
-							<label htmlFor="matric">Hours:</label>
+							<label htmlFor="matric">Hour:</label>
 							<input
 								type="number"
 								name="matric"
@@ -284,6 +328,9 @@ const Home = () => {
 							</button>
 						</div>
 					</div>
+
+					{/* background images */}
+					<Pattern />
 				</div>
 			)}
 		</>
@@ -291,3 +338,4 @@ const Home = () => {
 };
 
 export default Home;
+// 2022-
---
+++ pages/attendance/pattern.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Pattern = () => {
+	return (
+		<div>
+			<img
+				src="https://dashboard.render.com/static/media/blob-blue.f14299c7efcd24ca1e0169bcd2553812.svg"
+				alt=""
+				className="greeen absolute w-[50%] top-0 left-0"
+			/>
+
+			<img
+				src="https://dashboard.render.com/static/media/blob-orange.e3aa62395ea58419906ab40a537b585d.svg"
+				alt=""
+				className="greeen absolute w-[50%] bottom-0  left-0"
+			/>
+
+			<img
+				src="https://dashboard.render.com/static/media/blob-purple-bottom.4df92f65bd518088944c557fb5afc84f.svg"
+				alt=""
+				className="greeen absolute w-[50%] bottom-0 right-0"
+			/>
+			<img
+				src="https://dashboard.render.com/static/media/blob-baby-blue.7868befcc1c6a66edc5ae9ddbb055ccb.svg"
+				alt=""
+				className="greeen absolute w-[50%] bottom-0 left-0"
+			/>
+			<img
+				src="https://dashboard.render.com/static/media/blob-green.e25117d72781024aa9c78fb382b5fd7f.svg"
+				alt=""
+				className="greeen absolute w-[20%] top-0 left-0"
+			/>
+			<img
+				src="https://dashboard.render.com/static/media/blob-yellow.ee90f0690b8874cca752a53b983ba4a2.svg"
+				alt=""
+				className="greeen absolute w-[50%] top-0 right-0"
+			/>
+			<img
+				src="https://dashboard.render.com/static/media/blob-yellow.ee90f0690b8874cca752a53b983ba4a2.svg"
+				alt=""
+				className="greeen absolute w-[50%] top-0 -right-16"
+			/>
+			<img
+				src="https://dashboard.render.com/static/media/blob-blue-top.2884289188dba8ff1e29a73a67506d9b.svg"
+				alt=""
+				className="greeen absolute w-[50%] top-0 right-0"
+			/>
+
+			<img
+				src="https://dashboard.render.com/static/media/blob-pink-top.670c664bcabd70ead0bfb32dca3881f9.svg"
+				alt=""
+				className="greeen absolute w-[10%] top-0 left-0 "
+			/>
+		</div>
+	);
+};
+
+export default Pattern;
